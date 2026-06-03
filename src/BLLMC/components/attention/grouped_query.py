cc49eccf2d3c0f937c9fa4f1c10a7f71cc87b670
Amzad Hossain Rafi <amzad.rafi@northsouth.edu>
2026-06-03 15:04:58 +0600

    fix : f16 traing bug fix

--- src/BLLMC/components/attention/grouped_query.py
+++ src/BLLMC/components/attention/grouped_query.py
@@ -33,9 +33,7 @@ class GroupedQueryAttention(nn.Module):
 
         self.wq = nn.Linear(config.emb_dim, config.emb_dim, bias=False)
         self.wo = nn.Linear(config.emb_dim, config.emb_dim, bias=False)
-        self.dropout = nn.Dropout(config.drop_rate)
-
-        # k cache
+        
         cos, sin = compute_rope_params(
             head_dim=self.head_dim,
             context_length=config.context_length,
@@ -51,14 +49,9 @@ class GroupedQueryAttention(nn.Module):
 
     def forward(self, x: torch.Tensor, use_cache=False):
         b, t, c = x.shape
-
-        # Projections
-        # Shape: (b, t, n_groups, head_dim) -> transpose to (b, n_groups, t, head_dim)
         k = self.wk(x).view(b, t, self.config.n_kv_heads, self.head_dim).transpose(1, 2)
         v = self.wv(x).view(b, t, self.config.n_kv_heads, self.head_dim).transpose(1, 2)
         q = self.wq(x).view(b, t, self.config.n_heads, self.head_dim).transpose(1, 2)
-
-        # Apply RoPE to Query and Key
         q = apply_rope(q, self.cos, self.sin, offset=self.ptr_current_pos)
         k = apply_rope(k, self.cos, self.sin, offset=self.ptr_current_pos)
 
@@ -75,18 +68,14 @@ class GroupedQueryAttention(nn.Module):
             self.k_cache, self.v_cache = None, None
             self.ptr_current_pos = 0
 
-        # Repeat key/value groups to match the number of query heads
         repeats = self.config.n_heads // self.config.n_kv_heads
         k_head = torch.repeat_interleave(key_base, repeats, dim=1)
         v_head = torch.repeat_interleave(val_base, repeats, dim=1)
 
-        # Matmul: (b, n_heads, t, head_dim) @ (b, n_heads, head_dim, seq_len_K) -> (b, n_heads, t, seq_len_K)
         score = q @ k_head.transpose(-2, -1)
 
         num_tokens_Q = q.shape[-2]
         num_tokens_K = k_head.shape[-2]
-
-        # Dynamic causal mask based on absolute position tracking
         if use_cache:
             q_positions = torch.arange(
                 self.ptr_current_pos,
@@ -102,14 +91,11 @@ class GroupedQueryAttention(nn.Module):
         k_positions = torch.arange(num_tokens_K, device=k.device, dtype=torch.long)
 
         q_mask = q_positions[:, None] < k_positions[None, :]
-        atten_score = score.masked_fill_(q_mask, -torch.inf)
+        attention_score = score / (k_head.shape[-1] ** 0.5)
+        attention_score = attention_score.masked_fill_(q_mask, -torch.inf)
 
-        attention_weights = torch.softmax(
-            atten_score / (k_head.shape[-1] ** 0.5), dim=-1
-        )
-        attention_weights = self.dro
-        # Context computationpout(attention_weights)
-
+        attention_weights = torch.softmax(attention_score, dim=-1)
+    
         context = (attention_weights @ v_head).transpose(1, 2).contiguous()
         context = context.view(b, num_tokens_Q, self.config.emb_dim)
 
